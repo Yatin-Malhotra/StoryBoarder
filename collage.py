5c951160e7f17351b7d9e246fa224f3a5b97f11f
Yatin <127234342+Yatin-Malhotra@users.noreply.github.com>
2023-03-25 19:50:25 -0400

    Update collage.py

--- collage.py
+++ collage.py
@@ -1,27 +1,61 @@
 from PIL import Image
 
 def collage(num_images):
-    # Open the input images
-    input_images = []
 
-    for i in range(num_images):
-        input_images.append(f"storyboardimage{i + 1}.jpg")
+    image_size = (400, 400)
+    
+    if (num_images % 2 == 0):
+        num_images_per_row = num_images // 2
+        num_images_per_col = num_images // 2
 
-    # Open the input images and resize them to the same size (optional)
-    images = [Image.open(file).resize((400, 400)) for file in input_images]
+        input_images = []
 
-    # Create a new image with the desired size and mode (RGB or RGBA)
-    collage_width = num_images * images[0].width
-    collage_height = images[0].height
-    collage_mode = "RGB"
-    collage_image = Image.new(collage_mode, (collage_width, collage_height))
+        # Open the input images
+        for i in range(num_images):
+            input_images.append(f"storyboardimage{i + 1}.jpg")
 
-    # Paste the input images into the collage image
-    for i, image in enumerate(images):
-        x_offset = i * image.width
-        collage_image.paste(image, (x_offset, 0))
-        # Save the collage image with a name based on the counter
-        collage_image.save(f"collage_{i+1}.jpg")
+        # Open the input images and resize them to the same size (optional)
+        images = [Image.open(file).resize((400, 400)) for file in input_images]
 
-    # Save the final collage image to a file
-    collage_image.save("collage.jpg")
+        # Create a new image for the collage
+        collage_size = (image_size[0] * num_images_per_row, image_size[1] * num_images_per_col)
+        collage = Image.new("RGB", collage_size)
+
+        for i in range(num_images_per_row):
+            for j in range(num_images_per_col):
+                index = i + j * num_images_per_row
+                if index < len(images):
+                    x = i * image_size[0]
+                    y = j * image_size[1]
+                    collage.paste(images[index], (x, y))
+
+        # Save the final collage image to a file
+        collage.save("collage.jpg")
+    else:
+        num_images_per_row = (num_images // 2) + 1
+        num_images_per_col = num_images // 2
+
+        input_images = []
+
+        # Open the input images
+        for i in range(num_images):
+            input_images.append(f"storyboardimage{i + 1}.jpg")
+
+        # Open the input images and resize them to the same size (optional)
+        images = [Image.open(file).resize((400, 400)) for file in input_images]
+
+        # Create a new image for the collage
+        collage_size = (image_size[0] * num_images_per_row, image_size[1] * num_images_per_col)
+        collage = Image.new("RGB", collage_size)
+
+        for i in range(num_images_per_row):
+            for j in range(num_images_per_col):
+                index = i + j * num_images_per_row
+                if index < len(images):
+                    x = i * image_size[0]
+                    y = j * image_size[1]
+                    collage.paste(images[index], (x, y))
+
+        # Save the final collage image to a file
+        collage.save("collage.jpg")
+
