44ff9c3af1b6baf418a324c6516acee384eedede
Michael W <94869671+MichaelWK04@users.noreply.github.com>
2023-03-25 18:49:03 -0400

    Add files via upload

---
+++ custom_story.py
@@ -0,0 +1,21 @@
+import openai
+openai.api_key = "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"
+
+from PIL import Image
+import requests
+from io import BytesIO
+
+def Custom_Script(script: str, genre: str) -> str:
+    
+    text_prompt = "Write me a movie script for the genre " + genre + " with the description of: " + script
+
+    chatgpt_response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": "user", "content": text_prompt}],
+            temperature=0.1,
+            max_tokens=2000,
+            top_p=0.95)
+
+    response = chatgpt_response['choices'][0]['message']['content'].strip()
+
+    return response
