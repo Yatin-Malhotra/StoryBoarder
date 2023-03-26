90979d569a744ea713795ebe76fc9f3cf1fd5c9a
sparthan9 <128233603+sparthan9@users.noreply.github.com>
2023-03-26 18:26:10 -0400

    Add files via upload

---
+++ split_lines.py
@@ -0,0 +1,9 @@
+def read_script(script):
+    lines = script.splitlines()
+    return lines
+
+'''
+script = "This is line 1.\nThis is line 2.\nThis is line 3."
+lines = read_script(script)
+print(lines)
+'''
