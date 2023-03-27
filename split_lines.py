f1b14415db45433bdf9ff9b61700b313d62ae1e1
Yatin <127234342+Yatin-Malhotra@users.noreply.github.com>
2023-03-26 21:45:58 -0400

    Update split_lines.py

--- split_lines.py
+++ split_lines.py
@@ -1,11 +1,19 @@
 import audio as ad
 import movie as mv
+import images as im
 
-def read_script(script):
+def read_script(script, model):
     lines = script.splitlines()
-    length = len(lines)
+    updated_lines = []
+    for i in range(len(lines)):
+        if (lines[i].strip() != '' and lines[i].strip() != '\n'):
+            updated_lines.append(lines[i])
+    length = len(updated_lines)
 
     for i in range(length):
-        ad.generate_movie_clip(lines[i], i)
+        im.image_generator(updated_lines[i], model, i)
+
+    for i in range(length):
+        ad.generate_movie_clip(updated_lines[i], i)
     
     mv.generate_movie(length)
